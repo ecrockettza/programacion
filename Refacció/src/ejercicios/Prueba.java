a0ca60350b4f089105e6192e387b878bb5258592
Ethan <ethancrockett24@gmail.com>
2020-02-17 20:30:43 +0100

    Flujos de datos

---
+++ Refacció/src/ejercicios/Prueba.java
@@ -0,0 +1,14 @@
+package ejercicios;
+
+public class Prueba {
+	
+
+	public static void main(String[] args) {
+		int total = 0;
+		int contador=0;
+		total -= --contador;
+		System.out.println(total);
+	}
+	
+
+}
--- ejercicios/src/streams/Ejemplo_Escribir_y_Leer.java
+++ ejercicios/src/streams/Ejemplo_Escribir_y_Leer.java
@@ -1,19 +1,19 @@
 package streams;
 
 import java.io.*;
-import java.util.ArrayList;
 
 public class Ejemplo_Escribir_y_Leer {
 
 	public static void main(String[] args) {
 		Leer_y_Escribir archivo = new Leer_y_Escribir();
 		archivo.leer();
+		archivo.imprimir();
 		}
 		
 }
 	
 class Leer_y_Escribir{
-	  ArrayList <String> l1=new ArrayList <String>();
+
 	public void escribir() {
 		try {
 			FileWriter archivo = new FileWriter("C:/Users/ecrockett/Desktop/texto.txt", true);
@@ -27,45 +27,85 @@ class Leer_y_Escribir{
 				}
 	}
 	
+		
+	String palabras[] = new String[200];
+	char[] vaciochar;
+	
 	public void leer() {
 		try {
 			FileReader archivo= new FileReader("C:/Users/ecrockett/Desktop/texto.txt");
 			BufferedReader archivoBuffer = new BufferedReader(archivo);
 			String vacio="";
 			
+			int x=0;
+			
 			while (vacio!=null) {
 				vacio = archivoBuffer.readLine();
-				char[] vaciochar = vacio.toCharArray();
-				int i, j;
-				char aux;
-				for (i = 1; i <vaciochar.length; i ++) {
-					for (j = 0; j <vaciochar.length - i - 1; j ++) {
-						if(vaciochar [j + 1] <vaciochar [j]) {
-							aux = vaciochar [j + 1];
-							vaciochar [j + 1] = vaciochar [j];
-							vaciochar [j] = aux;
-				            }
-						}
-					}
-
- 				if (vacio!=null) 
-				{
-					System.out.println(vaciochar);
-				}
+				palabras[x] = vacio;
+				vaciochar = palabras[x].toCharArray();
+				x++;
 			}
 			archivoBuffer.close();
 		} 
+		
 		catch (FileNotFoundException e) { 
 		// Exception por si no encuentra el archivo
 		System.out.println("No es troba el fitxer");
 		} 
+		
 		catch (IOException e) { 
 		// Error si hay un fallo en la lectura
 		System.out.println("No es pot llegir el fitxer");
 		}
+		
 		catch (NullPointerException e) { 
-			// Error si hay un fallo en la lectura
+			// Salta cuando encuentra un nulo
 		}
 	}
+	static boolean booleano;
+	static int comprovador = 0;
+	static int contadorDeArray;
+	public static boolean ArrayOrder(char[] String1, char[] String2) { //metodo para hacer comparaciones
+		
+		
+		do {
+		if (String1[contadorDeArray] > String2[contadorDeArray]) { //metemos el array con su contador y miramos si es mayor el 1 que el 2
+		comprovador = 2; // si lo es pasa al comprovador
+		booleano = true;} // y el boolean es true
+		if (String1[contadorDeArray] < String2[contadorDeArray]) {
+		comprovador = 1; // si lo es pasa al comprovador
+		booleano = false;} // y el boolean es false
+		if (String1[contadorDeArray] == String2[contadorDeArray]) {  //si son iguales sale de do while y devuelve el booleano
+		comprovador = 0;}
+
+		contadorDeArray++;
+
+		}while(comprovador == 0);
+		contadorDeArray = 0;
+		return booleano;
+		}
+	
+	public void imprimir(){ 
+		int i, j;
+		int y= 0;
+		String aux;
+		
+		for (i = 0; i <vaciochar.length-1; i ++) {
+			for (j = 0; j <vaciochar.length - i - 1; j ++) {
+				
+				if(ArrayOrder(char[] String1, char[] String2)) {
+					aux = palabras [j + 1];
+					palabras [j + 1] = palabras [j];
+					palabras [j] = aux;
+		            }
+				}
+			}
+		
+		
+		while(y!=5){
+			System.out.println(palabras[y]);
+			y++;
+			}
+		}
 }
 
